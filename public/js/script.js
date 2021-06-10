dbb33d2fe8dd93463e6dc07e13e4244e28a140d2
James  Wallace <jameswallace@Jamess-MBP.lan1>
2021-06-10 11:28:56 -0400

    added insert climber jquery method

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,38 @@ $( document ).ready(function() {
                 console.log("I guess calling the API failed bruh")
                 console.log(error)
             }
-        })
+        });
+    });
+
+    $( "#insertClimber" ).click(function() {
+        console.log("that insert climber buttoon was clicked bruh");
+
+        const email = $( "#email" ).val()
+        const username = $( "#username" ).val()
+        const firstname = $( "#firstname" ).val()
+        const lastname = $( "#lastname" ).val()
+
+        const URL = "https://b1rzvmzxb5.execute-api.us-east-1.amazonaws.com/dev/climbers"
+
+        $.ajax({
+            url: URL,
+            type: "PUT",
+            headers: {
+                "email": email,
+                "username": username,
+                "first_name": firstname,
+                "last_name": lastname,
+                "Access-Control-Allow-Origin": "http://senddbbucket.s3-website-us-east-1.amazonaws.com"
+            },
+            success: function(result) {
+                console.log("I guess the call to the post worked bruh")
+                console.log(result)
+            },
+            error: function(error) {
+                console.log("I guess the call to the post climber failed bruh")
+                console.log(error)
+            }
+        });
     });
 });
 
