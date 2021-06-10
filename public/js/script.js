802243e51cf256eae07d0ed307dfbde827ff39d2
James  Wallace <jameswallace@Jamess-MBP.lan1>
2021-06-09 23:07:01 -0400

    test this bruh

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,25 @@
-alert('Hello, world!')
-console.log('what is happening!')
+$( document ).ready(function() {
+    console.log("this is from this jquery method or whatever");
+
+    $( "#test" ).click(function() {
+        console.log("that test button was clicked bruh");
+
+        const url = "https://b1rzvmzxb5.execute-api.us-east-1.amazonaws.com/dev/climbers"
+        $.ajax({
+            url: URL,
+            type: "GET",
+            headers: {
+                "climber_id": "2021-06-08 18:48:55.548080T1621967203918165956"
+            },
+            success: function(result) {
+                console.log("I guess we called the API?")
+                console.log(result)
+            },
+            error: function(error) {
+                console.log("I guess calling the API failed bruh")
+                console.log(error)
+            }
+        })
+    });
+});
+
